fa1c80c65359fcbaf000ef1c096b25dba0f5ea91
luccas-Cardoso <107223880+luccas-Cardoso@users.noreply.github.com>
2022-06-09 22:47:18 -0300

    Update OlaMundo.java

--- src/main/java/olamundo/OlaMundo.java
+++ src/main/java/olamundo/OlaMundo.java
@@ -4,7 +4,7 @@ public class OlaMundo {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Vai falhar! Coloque a mensagem correta. Ver teste.");
+		System.out.println("Sou UFABC!");
 
 	}
 
